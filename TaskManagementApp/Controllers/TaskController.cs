2f5a340d3867896b7e7ef9946a46ac03cdc8d572
adham <you@example.com>
2024-09-22 14:06:39 +0300

    adham

---
+++ TaskManagementApp/Controllers/TaskController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskManagementApp.Models;
+using TaskManagementApp.Repository;
+
+namespace TaskManagementApp.Controllers
+{
+    public class TaskController : Controller
+    {
+        private readonly ITaskRepository taskRepo;
+
+        public TaskController(ITaskRepository taskRepo)
+        {
+            this.taskRepo = taskRepo;
+        }
+        public IActionResult Index()
+        {
+            return View(taskRepo.GetAll());
+        }
+        public IActionResult AddTask()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddTask(UserTask newTask)
+        {
+            if (ModelState.IsValid)
+            {
+                taskRepo.Insert(newTask);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                ModelState.AddModelError("UserId", "User Id Not Valid");
+                
+                return View(newTask);
+            }
+        }
+
+        public IActionResult Edit(int Id)
+        {
+            UserTask task = taskRepo.GetById(Id);
+            return View(task);
+        }
+        [HttpPost]
+        public IActionResult Edit(int Id,UserTask newtask)
+        {
+            if (ModelState.IsValid)
+            {
+                taskRepo.Update(Id, newtask);
+                return RedirectToAction("Details", new {Id=newtask.TaskId});
+            }
+            return View(newtask);
+        }
+
+        public IActionResult Details(int Id)
+        {
+            return View(taskRepo.GetById(Id));
+        }
+        public IActionResult Delete(int Id)
+        {
+            taskRepo.Delete(Id);
+            return RedirectToAction("Index");
+        }
+
+        public IActionResult ConfirmDelete(int Id)
+        {
+
+            return View(Id);
+        }
+    }
+}
---
+++ TaskManagementApp/Repository/TaskRepository.cs
@@ -0,0 +1,63 @@
+﻿using TaskManagementApp.Data;
+using TaskManagementApp.Models;
+
+namespace TaskManagementApp.Repository
+{
+    public class TaskRepository : ITaskRepository
+    {
+        private readonly ApplicationDbContext context;
+
+        public TaskRepository(ApplicationDbContext context)
+        {
+            this.context = context;
+        }
+        public void Delete(int Id)
+        {
+            UserTask task= GetById(Id);
+            if (task != null)
+            {
+                context.Remove(task);
+                context.SaveChanges();
+            }
+            else
+                throw new NullReferenceException();
+        }
+
+        public List<UserTask> GetAll()
+        {
+            return context.Tasks.ToList();
+        }
+
+        public UserTask GetById(int Id)
+        {
+            return context.Tasks.FirstOrDefault(t=>t.TaskId==Id);
+        }
+
+        public List<UserTask> GetByUserId(string userId)
+        {
+            return context.Tasks.Where(t => t.UserId == userId).ToList();
+        }
+
+        public void Insert(UserTask task)
+        {
+            context.Add(task);
+            context.SaveChanges();
+        }
+
+        public void Update(int Id,UserTask task)
+        {
+            UserTask oldtask = GetById(Id);
+            if (oldtask != null)
+            {
+                oldtask.Title = task.Title;
+                oldtask.DueDate = task.DueDate;
+                oldtask.Description = task.Description;
+                oldtask.IsCompleted = task.IsCompleted;
+                oldtask.Priority = task.Priority;
+                context.SaveChanges();
+            }
+            else
+               throw new NullReferenceException();
+        }
+    }
+}
